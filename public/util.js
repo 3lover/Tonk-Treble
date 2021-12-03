e8c0165586b6026b8a73a1f97676da7c2c8a8b2f
Glitch (glitch-hello-website) <none>
2021-12-03 15:43:11 +0000

    🌈🎡 Checkpoint ./util.js:18138957/65 ./public/index.html:18138957/608 ./public/util.js:18138957/55

--- public/util.js
+++ public/util.js
@@ -1,9 +1,9 @@
 // shows and hides html elements
 function toggleElements(show = [], hide = []) {
   for (let i in show) {
-    document.getElementById(show[i]).display = "block";
+    document.getElementById(show[i]).style.display = "block";
   }
   for (let i in hide) {
-    document.getElementById(hide[i]).display = "none";
+    document.getElementById(hide[i]).style.display = "none";
   }
 }
