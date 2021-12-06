2575284247b005b64ac538c976a1f840f97fc47a
Glitch (glitch-hello-website) <none>
2021-12-06 15:35:35 +0000

    🐏🛫 Checkpoint ./public/lobbymanager.js:18138957/625

--- public/lobbymanager.js
+++ public/lobbymanager.js
@@ -1,7 +1,26 @@
+/* global io */
+const socket = io();
+
+let Clients = [];
+class Client {
+  constructor(room) {
+    this.room = room;
+    this.host = false;
+  }
+}
+
 function joinlobby(room) {
-  checkLobbyUsers()
+  let c = new Client(room);
+  if (checkLobbyUsers() === 1) c.host = true;
+  Clients.push(c);
+  socket.emit("userJoined", {room: room, })
 }
 
 function checkLobbyUsers(room) {
-  
+  let users = 0;
+  for (let i in Clients) {
+    let c = Clients[i];
+    if (c.room === room) users++;
+  }
+  return users;
 }
