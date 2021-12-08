e78ee0bb5c3c80528b8da0b68872f0b1c4d7f28c
Glitch (glitch-hello-website) <none>
2021-12-08 15:27:19 +0000

    🏘⛩ Checkpoint ./public/index.html:18138957/676 ./public/util.js:18138957/114

--- public/util.js
+++ public/util.js
@@ -1,3 +1,5 @@
+/* global io */
+
 // shows and hides html elements
 function toggleElements(show = [], hide = []) {
   for (let i in show) {
@@ -6,4 +8,11 @@ function toggleElements(show = [], hide = []) {
   for (let i in hide) {
     document.getElementById(hide[i]).style.display = "none";
   }
+}
+
+// send join request
+function joinlobby(room) {
+  //let the server know a user joined
+  let socket = io();
+  socket.emit("userJoined", room);
 }
