a826c25f2dd230a40bd075814bfc2517f4e02f12
Glitch (glitch-hello-website) <none>
2021-12-09 15:38:00 +0000

    🐈🙋 Checkpoint ./index.js:18138957/1009

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ app.use(express.static('public'));
 class Entity {
   // set if it is bound to a client, the lobby, and the type of entity it is
   constructor(client = null, lobby = 0, type = "t") {
+    this.client = client;
+    this.lobby = lobby;
+    this.type = type;
     this.x = Math.floor(Math.random() * basesize);
     this.y = Math.floor(Math.random() * basesize);
   }
@@ -41,25 +44,29 @@ io.on("connection", socket => {
     players[room]++;
     let e = new Entity(socket.id, "t");
     entities.push(e);
-    console.log("player joined from " + room + ". Now there are " + players[room] + " players; " + entities.length)
+    console.log("player joined from " + room + ". Now there are " + players[room] + " players; " + entities.length + " " + socket.id)
   });
   
   // when a client leaves a room let us know and update
   socket.on("userLeft", room => {
-    players[entities[entities.indexOf(socket.id)].lobby]--;
-    entities.splice(entities.indexOf(socket.id), 1);
-    console.log("a player has left room " + room + ". " + players[room] + " players remain;")
+    //players[entities[entities.indexOf(socket.id)].lobby]--;
+    //entities.splice(entities.indexOf(socket.id), 1);
+    //console.log("a player has left room " + room + ". " + players[room] + " players remain;")
   });
   
   // when a client disconnects check if they are in a room
   socket.on('disconnect', () => {
     console.log("A User Has Left, " + entities.length + " " + socket.id);
     // check if we have a player instance and if so remove it
-    //if (entities.indexOf(socket.id) == undefined) 
-      console.log("entity id: " + entities.indexOf(socket.id));
-    //if (entities[entities.indexOf(socket.id)] == undefined) 
-    console.log("entity: " + entities[entities.indexOf(socket.id)]);//return;
-    //players[entities[entities.indexOf(socket.id)].lobby]--;
-    //entities.splice(entities.indexOf(socket.id), 1);
+    console.log("--");
+    for (let i in entities) {
+      console.log(entities[i].client);
+      if (entities[i].client === socket.id) {
+        console.log(socket.id + " removed")
+        players[entities[i].lobby]--;
+        entities.splice(i, 1);
+        break;
+      }
+    }
   });
 });
