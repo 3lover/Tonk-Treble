2e241c87a090770138410013cc43a5a980cd8a41
Glitch (tonk-treble) <none>
2021-12-10 16:17:22 +0000

    📉📯 Checkpoint ./public/util.js:18138957/225 ./public/index.html:18138957/104

--- public/util.js
+++ public/util.js
@@ -1,6 +1,7 @@
 /* global io */
-var socket = io();
+
 // shows and hides html elements
+var socket = io;
 function toggleElements(show = [], hide = []) {
   for (let i in show) {
     document.getElementById(show[i]).style.display = "block";
@@ -12,14 +13,12 @@ function toggleElements(show = [], hide = []) {
 // send join request
 function joinlobby(room) {
   //let the server know a user joined
-  //let socket = io();
   socket.emit("userJoined", room);
 }
 
 // emit when a player leaves
 function leavelobby(room) {
   //let the server know a user joined
-  //let socket = io();
   socket.emit("userLeft", room);
 }
 
