3af975bcc6351a9f06381033758a6322be7b60e3
Glitch (tonk-treble) <none>
2021-12-13 16:00:36 +0000

    ❣️🍛 Checkpoint ./public/util.js:18138957/633 ./public/index.html:18138957/59 ./index.js:18138957/2

--- public/util.js
+++ public/util.js
@@ -1,5 +1,15 @@
 /* global io */
-var socket = io();
+$(function() {
+  window.keydown(function (event) {
+    // Auto-focus the current input when a key is typed
+    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+      $currentInput.focus();
+    }
+    // When the client hits ENTER on their keyboard
+    if (event.which === 13) {
+    }
+  });
+/*//var socket = io();
 
 // shows and hides html elements
 function toggleElements(show = [], hide = []) {
@@ -23,6 +33,7 @@ function leavelobby(room) {
 }
 
 document.getElementById("leavelobby").onclick = function() {
-  alert("button clicked");
+  //alert("button clicked");
   //toggleElements(['centerContain', 'mainmenu'], ['gamemenu']);leavelobby(document.getElementById('server').value)
-}
+}*/
+});
